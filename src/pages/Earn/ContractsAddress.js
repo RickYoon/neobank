c5d5a69162752b0df535b5a25767d576a7da00ab
ilo yoon <iloyoon@MacBook-Pro-7.local>
2024-10-06 11:43:31 +0900

    add

--- src/pages/Earn/ContractsAddress.js
+++ src/pages/Earn/ContractsAddress.js
@@ -1,12 +1,13 @@
 export const Address = {
-    "VatContractAddress": "0x514066e1f24fDA9f2B379748f9C001C3475625cB",
-    "GemContractAddress": "0x345E902846aC3805719483d80D664ABa0B6aF40C",
-    "GemJoinContractAddress": "0xD40588c705B99d406B048629E4D8863a2434beF5",
-    "XSDContractAddress": "0xCef966528A867176BF3a575c9951f695e8eB77a3",
-    "XsdJoinContractAddress": "0x42FfAe0648A84c0AC72D012402f380ab511AcBb1",
-    "CDPManagerContractAddress": "0x0E492702CA0A2048e87A21CE3Ac7E11Be757af2b",
-    "OneClickContractAddress": "0x949FC90364a71BE0FAB2Ce5ADEEC60aef8FFc196",
-    "JugContractAddress": "0x09Fd469b3036E45Dad077Df411134Fb85218678e",
-    "SpotContractAddress": "0x92B7e50CE799e8E26dE7324b3a92e93Dbbdf554F",
-    "DaiExitManagerContractAddress": "0x82eD6Df7b8864C856e18d876e89c33F163eAc8ae"
+  "VatContractAddress": "0xce2fF2ff3B10adBF20bE5D287DFE8876C5909950",
+  "GemContractAddress": "0x1CE16390FD09040486221e912B87551E4e44Ab17",
+  "GemJoinContractAddress": "0xACa8F1491a9EA47e3401E066F16723B85AC79357",
+  "XSDContractAddress": "0x9941AcC3f70a6F41d726Fd3195D8dB5Dece3150D",
+  "XsdJoinContractAddress": "0x797d8B2e5154D1aa29698467860E75b573A56B88",
+  "CDPManagerContractAddress": "0x7dE842644b04BD370c23D92A4324f84fE92764CA",
+  "OneClickContractAddress": "0x3350f2E70C1514c9691e496b0080B4D035033022",
+  "JugContractAddress": "0xD70d59095a839518b9884bbF27F5F563286e7c68",
+  "SpotContractAddress": "0x6F2bA8EC599001c99250f8ACfebACc371c4b3482",
+  "DaiExitManagerContractAddress": "0x82eD6Df7b8864C856e18d876e89c33F163eAc8ae",
+  "supraOracleAddress": "0x5df499C9DB456154F81121282c0cB16b59e74C4b"
   }
